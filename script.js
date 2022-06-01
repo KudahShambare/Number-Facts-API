13711474e21f3a92ccd645d8865135becc026af2
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-06-01 15:04:55 +0200

    fetch removed from function

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 let numberFact = document.getElementById("numberFact");
+numberFact.style.display = "none";
+
 let inputValue = document.querySelector("input");
+
 let number = inputValue.value;
 let getFact = document.querySelector("button");
-
+let count = 5;
+let loading = "Loading in " + count + " seconds";
 /*Fetching From API*/
-function fetchData() {
-    fetch("http://numbersapi.com/" + number)
-      .then((response) => response.text())
-        .then((data) => {
-          data ===""?numberFact.innerHTML="Invalid input":
-          numberFact.innerHTML = data;
-      }).catch((error) => {
-          alert(error+" occurred")
-      })
 
-}
+fetch("http://numbersapi.com/" + number)
+  .then((response) => response.text())
+  .then((data) => {
+    data === ""
+      ? (numberFact.innerHTML = "Invalid input")
+      : (numberFact.innerHTML = data);
+  })
+  .catch((error) => {
+    alert(error + " occurred");
+  });
+
 /*Button Events*/
 
-getFact.addEventListener("click", fetchData)
-inputValue.addEventListener("change", () => {
+getFact.addEventListener("click", (e) => {
+  e.preventDefault();
+  if(number=""){
     window.location.reload();
-})
+  }
+  numberFact.style.display = "block";
+});
+inputValue.addEventListener("change", () => {
+  window.location.reload();
+
+
+});
